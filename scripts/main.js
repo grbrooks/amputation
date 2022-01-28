6be5dc47f5a3519371fb677417ac052fdf263849
grbrooks <grahambrooks@hotmail.co.uk>
2022-01-28 11:30:51 +0000

    --- Auto-Git Commit ---

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,14 @@
-window.onscroll = function() { myFunction() };
-var navbar = document.getElementById('navbar');
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add('sticky')
-    } else {
-        navbar.classList.remove('sticky')
-    }
-
-}
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
